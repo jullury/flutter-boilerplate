799b455b8b0de7f983ba05a72e9ee96b8bc7765a
Jullury <jullandriamazava@gmail.com>
2024-03-30 14:49:46 +0300

    chore(flutter): update dependencies

---
+++ app/android/app/src/main/kotlin/com/jullury/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jullury.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- app/test/fake_data.dart
+++
@@ -1,5 +0,0 @@
-import 'package:shared/shared.dart';
-
-const inputEmail = 'email';
-const inputPassword = 'password';
-const validationError = ValidationException(ValidationExceptionKind.invalidEmail);
